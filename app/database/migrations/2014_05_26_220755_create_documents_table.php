e21aeb38e910b4fd7c9d9268fe3832cf72611013
Youssef Kababe <youssef.kbe@gmail.com>
2014-05-26 23:08:32 +0100

    scaffold document

---
+++ app/database/migrations/2014_05_26_220755_create_documents_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateDocumentsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('documents', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('nomDocument');
+			$table->timestamps();
+		});
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('documents');
+	}
+
+}
