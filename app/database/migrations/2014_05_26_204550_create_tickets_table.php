60c3bf81f159a67c3b52439874feaa8674da1683
Youssef Kababe <youssef.kbe@gmail.com>
2014-06-05 17:04:14 +0100

    modify migrations

--- app/database/migrations/2014_05_26_204550_create_tickets_table.php
+++ app/database/migrations/2014_05_26_204550_create_tickets_table.php
@@ -14,6 +14,7 @@ class CreateTicketsTable extends Migration {
 	{
 		Schema::create('tickets', function(Blueprint $table) {
 			$table->increments('id');
+			$table->string('sujet');
 			$table->text('message');
 			$table->string('priorite');
 			$table->string('etat');
